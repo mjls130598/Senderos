0e96b0352b942d3882078cc8d784d3d0b1ec5c36
mjls130598 <mjls130598@gmail.com>
2021-04-07 19:23:49 +0200

    Arregla el modelo excursión Close #11

--- rutas_granada/models.py
+++ rutas_granada/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from datetime import datetime
 
-class Comentarios(models.Model):
+class Comentario(models.Model):
 	contenido = models.TextField()
 	autor     = models.CharField(max_length=120)
 	fecha     = models.DateTimeField(default=datetime.now())
 
-class Fotos(models.Model):
+class Foto(models.Model):
 	foto = models.CharField()
 	pie = models.CharField(max_length=120)
+
+class Excursion(models.Model):
+	nombre      = models.CharField(max_length=120)
+	descripción = models.TextField()
+	likes       = models.IntegerField(default=0)
+	visitas     = models.IntegerField(default=0)
+	tags        = models.ListField(models.CharField(max_length=20))
+	duración    = models.IntegerField(default=0)
+	comentarios = models.ForeignKey(Comentario, on_delete=models.CASCADE)
+	fotos 		= models.ForeignKey(Foto, on_delete=models.CASCADE) 
