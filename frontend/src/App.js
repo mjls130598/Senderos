be1db021ff1ea6e75b8258f26e2bb4c84018d15e
mjls130598 <mjls130598@gmail.com>
2021-06-22 13:33:43 +0200

    Crea la visualización base de la app React Close #46

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import logo from './static/images/logo.gif';
+import diputacion from './static/images/diputacion.png';
 import './App.css';
+import './static/css/base.css';
+import { Navbar, Container, Nav, Jumbotron, Image } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase font-weight-bold">
+          <Container fluid>
+            <Navbar.Collapse className="d-flex justify-content-center">
+              <Nav className="me-auto mb-2 mb-lg-0">
+                <Nav.Link className="active">Destinos</Nav.Link>
+                <Nav.Link className="active">Planea tu viaje</Nav.Link>
+                <Nav.Link className="active">Cosas que hacer</Nav.Link>
+                <Nav.Link href="/" className="active"><Image src={logo} thumbnail/></Nav.Link>
+                <Nav.Link className="active">Agenda cultural</Nav.Link>
+                <Nav.Link className="active">Área profesional</Nav.Link>
+                <Nav.Link className="active">Mi Granada</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+
+      <body>
+        <Jumbotron className="text-center">
+          <h4 className="font-weight-bold">Turismo rural, activo y de naturaleza</h4>
+        </Jumbotron>
+      </body>
+
+      <footer className="bg-dark text-white">
+        <Container footer>
+          <Image src={diputacion} alt="Diputación de Granada"/>
+          <div className="float-right">
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="https://www.turgranada.es/patronato-provincial-de-turismo/" className="text-white">Patronato Provincial de Turismo Diputación de Granada</a>
+              </li>
+              <li>
+                Cárcel Baja, 3. 18001. Granada
+              </li>
+              <li>
+                Tel: +34 958 24 71 46
+              </li>
+              <li>
+                Fax: +34 958 24 71 29
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </footer>
     </div>
   );
 }
