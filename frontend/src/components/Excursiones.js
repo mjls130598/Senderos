21f06689fcc3777d99d4006043432c0a6c56af32
mjls130598 <mjls130598@gmail.com>
2021-06-23 11:04:43 +0200

    Realiza la visualización de todas las excursiones Close #44

---
+++ frontend/src/components/Excursiones.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Container, Row } from 'react-bootstrap';
+import '../static/css/cards.css'
+import Cards from './Excursion-cards';
+
+
+class Excursiones extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			excursiones: []
+		};
+	}
+
+	componentDidMount() {
+		const url = 'http://localhost:8000/api/excursiones/'
+
+		// una promise, que devuelve otra promise
+		fetch(url)
+			.then(res => res.json())
+			.then(res => {
+						console.log(res)
+						this.setState({excursiones:res})   // Se cambia el state y re-renderiza
+					})
+			.catch(error => {
+				alert('Error '+ error)
+			})
+	}
+
+	render() {
+
+		const listaExcursiones = this.state.excursiones.map(ex =>{
+			return (<Cards excursion = {ex}/>);
+		})
+
+		return (  
+			<Container className="excursiones">
+					<Row className="row-cols-3">
+					{
+						listaExcursiones
+					}
+					</Row>
+				</Container>
+		);
+	}
+}
+export default Excursiones;
